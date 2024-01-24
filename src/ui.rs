e790b9c96ec7f89057d0af33d0ba5cd16e7cc912
Mroik <mroik@delayed.space>
2024-01-24 22:53:46 +0100

    Add comment list rendering

--- src/ui.rs
+++ src/ui.rs
@@ -23,7 +23,7 @@ fn generate_layout(frame: &Frame) -> (Rect, Rect, Rect) {
 pub fn view(model: &Model, frame: &mut Frame) {
     let (overview, comments, viewer) = generate_layout(frame);
 
-    let list = List::new(model.overview.iter().map(|item| item.title.clone()))
+    let threads_list = List::new(model.overview.iter().map(|item| item.title.clone()))
         .block(
             Block::default()
             .title("Overview")
@@ -37,7 +37,24 @@ pub fn view(model: &Model, frame: &mut Frame) {
         .highlight_symbol(">>");
     let mut state = ListState::default();
     state.select(Some(model.selected_thread.into()));
+    frame.render_stateful_widget(threads_list, overview, &mut state);
 
-    frame.render_stateful_widget(list, overview, &mut state);
+    let thread = model.data.data.get(model.selected_thread as usize).unwrap();
+    let comment_list = List::new(thread.comments.iter().map(|item| item.author.clone()))
+        .block(
+            Block::default()
+            .title("Comments")
+            .borders(Borders::ALL))
+        .style(
+            Style::default()
+            .fg(Color::White))
+        .highlight_style(
+            Style::default()
+            .bg(Color::LightBlue))
+        .highlight_symbol(">>");
+    let mut state = ListState::default();
+    state.select(Some(model.data.selected_comment.into()));
+    frame.render_stateful_widget(comment_list, comments, &mut state);
+
     // TODO
 }
